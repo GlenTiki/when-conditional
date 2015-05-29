530d2f9b063f9783b39988257fec645261ed4d99
Glen Keane <glenkeane.94@gmail.com>
2015-05-29 14:19:13 +0100

    fix missing dep

--- when.js
+++ when.js
@@ -1,6 +1,6 @@
 // the require('setImmediate') adds a setImmediate polyfill to the global
 // scope. (this is for backwards and browser compatibility)
-require('setImmediate');
+require('setimmediate');
 
 function when(condition, code){
 	if(condition()){
